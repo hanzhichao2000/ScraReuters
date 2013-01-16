8604c41de41729a09827c3584a62cfabf48500c2
hanzhichao2000@gmail.com <andy@andy-mac.local>
2013-01-16 20:43:08 +0800

    initialized the project

---
+++ ScraReuters/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for ScraReuters project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'ScraReuters'
+
+SPIDER_MODULES = ['ScraReuters.spiders']
+NEWSPIDER_MODULE = 'ScraReuters.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ScraReuters (+http://www.yourdomain.com)'

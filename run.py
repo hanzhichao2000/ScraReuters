e8e6c2024f9c89ce8ca4ca96aa069d3f819647a3
hanzhichao2000@gmail.com <andy@andy-mac.local>
2013-01-19 21:43:09 +0800

    fixed step-based raw-crawler

--- run.py
+++ run.py
@@ -3,22 +3,32 @@ TODO
 - call issue
 '''
 
-import subprocess
 from datetime import datetime, timedelta
+import json
+import os
+import subprocess
+import time
 
 def crawl_day_page(year=2008, month=1, day=1):
     current_date = datetime(year, month, day)
     delta = timedelta(1)
-    while current_date < datetime(2013, 12, 1):
+    while current_date < datetime(2013, 1, 1):
         cd = current_date
         print 'Fetching %d-%02d-%02d'%(cd.year, cd.month, cd.day)
-        subprocess.call('scrapy crawl RawSpider \
-                            -a year=%d -a month=%d -a day=%d \
-                            -o news/reuters%d%02d%02d.json -t json 2> log'%(
-                            cd.year, cd.month, cd.day,
-                            cd.year, cd.month, cd.day),
-                         shell=True)
-        current_date += delta
+        outfile = 'reuters/%04d%02d%02d.json'%(cd.year, cd.month, cd.day)
+        if not os.path.exists(outfile):
+            subprocess.call('scrapy crawl RawSpider \
+                                -a year=%d -a month=%d -a day=%d \
+                                -o %s -t json 2>NULL'%(
+                                cd.year, cd.month, cd.day, outfile),
+                            shell=True)
+        try:
+            news = json.load(open(outfile))
+            current_date += delta
+        except Exception:
+            print '- Error!'
+            os.remove(outfile)
+            time.sleep(10)
 if __name__ == '__main__':
     crawl_day_page()
 
